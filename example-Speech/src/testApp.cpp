5ece45aea5d5462166d75992ac1dbc71b106ec64
Josh <joshua.noble@frogdesign.com>
2014-03-03 14:33:56 -0800

    speech working

--- example-Speech/src/testApp.cpp
+++ example-Speech/src/testApp.cpp
@@ -4,12 +4,20 @@
 void testApp::setup(){
 
 	kinect.initSensor(0);
-	//string grammarPath = ofToDataPath("grammar/en-US.grxml", true);
-	string grammarPath = "C:\\en-US.grxml";
+	string grammarPath = ofToDataPath("grammar\\SpeechBasics-D2D.grxml", true);
+	//string grammarPath = "C:\\SpeechBasics-D2D.grxml";
 	kinect.setSpeechGrammarFile(grammarPath);
-	kinect.initAudio();
+	//kinect.initAudio();
 	kinect.initSpeech();
 	kinect.start();
+
+	ofAddListener(ofxKCBSpeechEvent::event, this, &testApp::speechEvent);
+
+}
+
+void testApp::exit()
+{
+	kinect.stop();
 }
 
 //--------------------------------------------------------------
