869189f4e6b473b240845891a74228b46a892744
Josh <joshua.noble@frogdesign.com>
2014-05-25 22:26:18 -0700

    adding face tracking example

---
+++ example-Face/src/main.cpp
@@ -0,0 +1,19 @@
+
+#include "testApp.h"
+
+//#include "ofGLProgrammableRenderer.h"
+
+//========================================================================
+int main( ){
+
+	//ofPtr<ofBaseRenderer> renderer(new ofGLProgrammableRenderer(true));
+	//ofSetCurrentRenderer(renderer);
+
+	ofSetupOpenGL(640,480, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new testApp());
+
+}
---
+++ example-Face/src/testApp.cpp
@@ -0,0 +1,80 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+	kinect.initFaceTracking();
+	kinect.start();
+}
+
+void testApp::exit()
+{
+	kinect.stop();
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	kinect.update();
+	if(kinect.isFaceNew()) {
+		face = kinect.getFaceData();
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+
+	ofBackground(100, 100, 100);
+	ofSetColor(0, 255, 0 );
+
+	ofNoFill();
+	ofRect( face.rect ); // draw the bounding box of the face
+
+	ofSetColor(255, 255, 255 );
+	face.mesh.drawWireframe(); // draw the mesh of the face
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
